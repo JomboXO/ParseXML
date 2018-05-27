c551e5df04b589778dd9c826bfa3dffc6620dba5
Татьяна <tanya0394@mail.ru>
2018-05-27 20:20:18 +0300

    Imports have been changed.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,10 @@
 package com.company;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.List;
-
+import org.w3c.dom.*;
+import org.xml.sax.*;
+import javax.xml.parsers.*;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
@@ -46,8 +36,8 @@ public class Main {
         sortByProject();
         sortByRoleName();
 
+        /* Result */
         System.out.println(createNewXml());
-
     }
 
     private static void parseXml(String string) {
